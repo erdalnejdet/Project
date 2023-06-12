6ccd5e28cd6e8d07c0f19bdc2ba7e419612e080e
Nejdet erdal <erdalnejdet1@gmail.com>
2023-06-12 15:16:47 +0300

    Added with gulp

--- src/js/a_arina.js
+++ src/js/a_arina.js
@@ -1,11 +1,12 @@
 
+  
 
 $('.mobile-menu-burger-container').click(function () {
-    $(this).toggleClass('burger-active');
-    $('body').toggleClass('scroll-disabled');
-    $('.mobile-header-menu').toggleClass('mobile-header-menu-active');
+  $(this).toggleClass('burger-active');
+  $('body').toggleClass('scroll-disabled');
+  $('.mobile-header-menu').toggleClass('mobile-header-menu-active');
 
-    $('.mobile-header-menu ul').find('ul').slideUp();
+  $('.mobile-header-menu ul').find('ul').slideUp();
 });
 
 var modal = document.querySelector(".form");
@@ -14,29 +15,29 @@ var closeButton = document.querySelector(".close");
 var giveup = document.querySelector(".give-up");
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+  modal.classList.toggle("show-modal");
 }
 
 function windowOnClick(event) {
-    if (event.target === modal || event.target === document.body) {
-      event.preventDefault();
-      if (confirm("Emin misiniz?")) {
-        toggleModal();
-      }
-    }
-  }
-  
-  
-
-  function preventDefaultSubmit(event) {
+  if (event.target === modal || event.target === document.body) {
     event.preventDefault();
     if (confirm("Emin misiniz?")) {
       toggleModal();
-    } else {
-      // Kapatma işlemi gerçekleştirme
     }
   }
-  
+}
+
+
+
+function preventDefaultSubmit(event) {
+  event.preventDefault();
+  if (confirm("Emin misiniz?")) {
+    toggleModal();
+  } else {
+    // Kapatma işlemi gerçekleştirme
+  }
+}
+
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 giveup.addEventListener("click", preventDefaultSubmit);
@@ -47,26 +48,27 @@ giveup.addEventListener("click", preventDefaultSubmit);
 const savebutton = document.querySelector(".save");
 
 savebutton.addEventListener("click",function(e){
-    ekleKayit();
+  ekleKayit();
+
 
 });
 
 // Sayfa yüklendiğinde kaydedilmiş verileri yükle
 window.onload = function() {
-    loadSavedData();
-  };
-  
-  // Kaydedilmiş verileri yükle
-  function loadSavedData() {
-    var savedData = localStorage.getItem("kayitlar");
-    if (savedData) {
-      var kayitlar = JSON.parse(savedData);
-      for (var i = 0; i < kayitlar.length; i++) {
-        addKayitRow(kayitlar[i]);
-      }
+  loadSavedData();
+};
+
+// Kaydedilmiş verileri yükle
+function loadSavedData() {
+  var savedData = localStorage.getItem("kayitlar");
+  if (savedData) {
+    var kayitlar = JSON.parse(savedData);
+    for (var i = 0; i < kayitlar.length; i++) {
+      addKayitRow(kayitlar[i]);
     }
   }
-  
+}
+
 // Yeni kayıt ekle
 function ekleKayit() {
   event.preventDefault();
@@ -100,43 +102,45 @@ function ekleKayit() {
   document.getElementById("social").value = "";
   document.getElementById("name").value = "";
   document.getElementById("explanation").value = "";
+
+  alert("Kayıt başarıyla eklendi!");
 }
 
 // Kayıtları kontrol et
 function kayitVarMi(kayit) {
-  var savedData = localStorage.getItem("kayitlar");
-  if (savedData) {
-    var kayitlar = JSON.parse(savedData);
-    for (var i = 0; i < kayitlar.length; i++) {
-      if (
-        kayitlar[i].social === kayit.social &&
-        kayitlar[i].name === kayit.name &&
-        kayitlar[i].explanation === kayit.explanation
-      )
-      {
-        return true;
-      }
+var savedData = localStorage.getItem("kayitlar");
+if (savedData) {
+  var kayitlar = JSON.parse(savedData);
+  for (var i = 0; i < kayitlar.length; i++) {
+    if (
+      kayitlar[i].social === kayit.social &&
+      kayitlar[i].name === kayit.name &&
+      kayitlar[i].explanation === kayit.explanation
+    )
+    {
+      return true;
     }
   }
-  return false;
+}
+return false;
 }
 
-  // Yeni kayıt satırını tabloya ekle
-  function addKayitRow(kayit) {
-    var table = $('#case').DataTable();
-    table.row.add([kayit.social, kayit.name, kayit.explanation]).draw();
+// Yeni kayıt satırını tabloya ekle
+function addKayitRow(kayit) {
+  var table = $('#case').DataTable();
+  table.row.add([kayit.social, kayit.name, kayit.explanation]).draw();
+}
+
+// Kayıt verilerini yerel depolamaya kaydet
+function saveKayitData(kayit) {
+  var savedData = localStorage.getItem("kayitlar");
+  var kayitlar = [];
+
+  if (savedData) {
+    kayitlar = JSON.parse(savedData);
   }
-  
-  // Kayıt verilerini yerel depolamaya kaydet
-  function saveKayitData(kayit) {
-    var savedData = localStorage.getItem("kayitlar");
-    var kayitlar = [];
-  
-    if (savedData) {
-      kayitlar = JSON.parse(savedData);
-    }
-  
-    kayitlar.push(kayit);
-    localStorage.setItem("kayitlar", JSON.stringify(kayitlar));
-  }
-  
+
+  kayitlar.push(kayit);
+  localStorage.setItem("kayitlar", JSON.stringify(kayitlar));
+}
+
